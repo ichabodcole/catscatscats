554edc45754fa6ac69210dc39dbf0323a0757ae7
ichabodcole <ichabodcole@gmail.com>
2013-03-03 13:22:48 -0800

    Refactoring create button handler event

--- public/event_handlers.js
+++ public/event_handlers.js
@@ -2,23 +2,26 @@
 $(function(){
     $("button").addClass("btn btn-primary");
 
+    $("#btn-create").click(function(e){
+        var subject = $("#txt-subject").val();
+        var content = $("#txt-content").val();
+        var note = new Note({subject: subject, content: content});
+        noteCollection.add(note);
+        note.create().done(function(data){
+            buildAlertDisplay({cid: note.cid, id: note.id, subject: note.subject, content: note.content},"success");
+            refresh(noteCollection);
+        });
+    });
+
     $("#btn-update").click(function(e){
         var id = Number($("#sel-noteid").val());
         var subject = $("#txt-subject").val();
         var content = $("#txt-content").val();
         var note = noteCollection.getByCid(id);
         note.set({subject: subject, content: content});
-        note.update();
-        buildAlertDisplay({cid: note.cid, id: note.id, subject: note.subject, content: note.content},"success");
-    });
-
-    $("#btn-create").click(function(e){
-        var subject = $("#txt-subject").val();
-        var content = $("#txt-content").val();
-        var data = {subject: subject, content: content};
-        var note = noteCollection.add(new Note(data));
-        buildAlertDisplay({cid: note.cid, id: note.id, subject: note.subject, content: note.content},"success");
-        refresh(noteCollection);
+        note.update().done(function(data){
+            buildAlertDisplay({cid: note.cid, id: data.id, subject: data.subject, content: data.content},"success");
+        });
     });
 
     $("#sel-noteid").change(function(e){
