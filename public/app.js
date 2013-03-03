f78ad5c2b42dbe770216f11c008a9afe54d9b74a
ichabodcole <ichabodcole@gmail.com>
2013-03-03 14:21:30 -0800

    Remove commented out code

--- public/app.js
+++ public/app.js
@@ -1,15 +1,7 @@
 $(function(){
     noteCollection = new NoteCollection(Note);
-    // var note2 = new Note();
-    // note2.set({id: 2, subject: "Test subject 2", content: "Test content 2"});
-
-    // noteCollection.add(note1);
-    // noteCollection.add(note2);
 
     noteCollection.sync().done(function(){
-        // var note1 = new Note({subject: "Test subject 1", content: "Test content 1"});
-        // noteCollection.add(note1);
         refresh(noteCollection);
     });
-    //Fill a note from the server.
 });
